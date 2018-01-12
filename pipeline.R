642b1aa9e9c7222bb98329946bfe57a8a403260a
Matthew Epstein <mepstein9@yahoo.com>
2018-01-12 10:17:42 -0500

    replaced pipeline.R with pipeline_v1.R and added +x permissions

--- pipeline.R
+++ pipeline.R
@@ -1,51 +1,65 @@
 #!/usr/bin/env Rscript
 
+
+args = commandArgs(trailingOnly=TRUE)
+if (length(args) != 1) {
+  stop("One argument must be supplied (the interval size)")
+}
+interval <- args[1]
+
 options(echo=TRUE, warn=1)
-
 library(DiffBind)
-
-samples <- read.csv("input.csv")
 data <- dba(sampleSheet="input.csv")
+date <- format(Sys.Date(), format="%Y-%m-%d")
 
-pdf("input.v1.pdf")
+
+pdf("input.pdf", paper="a4")
 plot(data, main="", sub="")
-title(main="input", cex.main=1)
-title(sub="2017-12-20-CBE", cex.sub=0.5)
+# need to look into this to see if the data can be fit to the page
+
+title(main="input.pdf", cex.main=1)
+title(sub=date, cex.sub=0.5) 
 dev.off()
 
-pdf("PCA-FACTOR.v1.pdf")
+
+pdf("PCA-FACTOR.pdf", paper="a4")
 dba.plotPCA(data, DBA_FACTOR, label=DBA_CONDITION)
 dev.off()
 
-data.0 <- dba.count(data)
-data.0.contrast <- dba.contrast(data.0, categories=DBA_CONDITION, minMembers=2)
-data.0.diffs <- dba.analyze(data.0.contrast)
 
-pdf("contrast.pdf")
-plot(data.0.diffs, contrast=1)
+if (interval == 0) {
+	counted <- dba.count(data)
+} else {
+	counted <- dba.count(data, summits=interval)
+}
+diffs <- dba.analyze(dba.contrast(counted, categories=DBA_CONDITION, minMembers=2))
+
+
+
+pdf("constrast.pdf", paper="a4")
+plot(diffs, contrast=1)
 dev.off()
 
-pdf("diffs.PCA-CONDITION.pdf")
-dba.plotPCA(data.0.diffs, contrast=1, label=DBA_FACTOR)
+pdf("PCA-CONDITION-diffs.pdf", paper="a4")
+dba.plotPCA(diffs, contrast=1, label=DBA_FACTOR)
 dev.off()
 
-data.0.peaks <- dba.report(data.0.diffs)
+peaks <- dba.report(diffs)
+peaks_df <- as(peaks,"data.frame")
+write.csv(peaks_df, file="constrast-loci.df.csv")
 
-peaks_df <- as(data.0.peaks,"data.frame")
-write.csv(peaks_df, file="contrast-loci.df.csv")
-
-pdf("peaks-histogram.pdf")
+pdf("peaks-histogram.pdf", paper="a4")
 hist(peaks_df$Fold, breaks=100)
 dev.off()
 
-pdf("MA.pdf")
-dba.plotMA(data.0.diffs)
+pdf("MA.pdf", paper="a4")
+dba.plotMA(diffs)
 dev.off()
 
-pdf("Volcano.pdf")
-dba.plotVolcano(data.0.diffs)
+pdf("Volcano.pdf", paper="a4")
+dba.plotVolcano(diffs)
 dev.off()
 
-pdf("Heatmap2.pdf")
-corvals <- dba.plotHeatmap(data.0.diffs, contrast=1, correlations=FALSE)
+pdf("Heatmap2.pdf", paper="a4")
+corvals <- dba.plotHeatmap(diffs, contrast=1, correlations=FALSE)
 dev.off()
